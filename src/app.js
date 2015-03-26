8e3bee74ee5f58884d5ed7309c0f9e14616709b8
Alex Weber <alexweber15@gmail.com>
2015-03-26 19:10:31 -0300

    reset to latest skeleton app

--- src/app.js
+++ src/app.js
@@ -1,40 +1,16 @@
 import {Router} from 'aurelia-router';
+import bootstrap from 'bootstrap';
 
 export class App {
-  static inject() {
-    return [Router];
-  }
-
+  static inject() { return [Router]; }
   constructor(router) {
     this.router = router;
-
     this.router.configure(config => {
       config.title = 'Aurelia';
       config.map([
-        {
-          route: ['', 'welcome'],
-          moduleId: 'welcome/welcome',
-          nav: true,
-          title: 'Welcome :)'
-        },
-        {
-          route: 'flickr',
-          moduleId: 'flickr/flickr',
-          nav: true,
-          title: 'Welcome to flickr'
-        },
-        {
-          router: 'todo',
-          moduleId: 'todo/todo',
-          nav: true,
-          title: 'ToDo'
-        }
-        //{
-        //  route: 'child-router',
-        //  moduleId: 'child-router',
-        //  nav: true,
-        //  title: 'Child Router'
-        //}
+        { route: ['','welcome'],  moduleId: './welcome',      nav: true, title:'Welcome' },
+        { route: 'flickr',        moduleId: './flickr',       nav: true },
+        { route: 'child-router',  moduleId: './child-router', nav: true, title:'Child Router' }
       ]);
     });
   }
